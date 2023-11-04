3f016ca0bc73209508bc0e8457c8a967bc5544c5
Son Dinh Thai <sondinh99999@gmail.com>
2023-11-04 23:45:18 +0700

    commit 2

--- quanlythuphidonggop/settings.py
+++ quanlythuphidonggop/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'quanlythuphidonggop.urls'
 
+LOGIN_REDIRECT_URL = '/'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- quanlythuphidonggop/urls.py
+++ quanlythuphidonggop/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('quanlythuphidonggop.urls')),  # Replace 'yourapp' with the name of your app
+]
+
+from django.contrib.auth import views as auth_views
+from myapp import views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
