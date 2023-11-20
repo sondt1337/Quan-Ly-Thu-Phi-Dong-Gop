c90d2703f5eec90dfcc3c27d93c00a5495ffe494
Son Dinh Thai <sondinh99999@gmail.com>
2023-11-21 02:19:30 +0700

    oke

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,46 +1,122 @@
-# Bài tập lớn IT3180 - Quản lý thu phí đóng góp
-Phần mềm Quản lý thu phí và đóng góp cho Tổ dân phố 7 phường La Khê là một giải pháp quản lý hiện đại giúp tổ chức theo dõi thông tin cộng đồng và thu tiền một cách hiệu quả.
+from pathlib import Path
 
-Với hơn 400 hộ gia đình, sinh viên thuê trọ và các khoản đóng góp đa dạng, phần mềm này giúp cán bộ kế toán lập danh sách, thu phí vệ sinh hàng năm và quản lý các đóng góp từ cộng đồng.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-Nó cũng cung cấp các công cụ thống kê để nắm bắt tình hình tài chính và tăng cường tính minh bạch trong quản lý. Dự án này sẽ nâng cao quản lý tổ dân phố thông qua công nghệ thông tin và hiệu quả hóa quy trình quản lý tài chính.
-## Thành viên nhóm 3
-| Thành viên                                              | MSSV     | Vai trò     | Nhiệm vụ           |
-| ------------------------------------------------------- | -------- | ----------- | ------------------ |
-| [Đinh Thái Sơn](https://github.com/vepiot/)             | 20210750 | Trưởng nhóm | Backend, Frontend  |
-| [Bùi Văn Huy](https://github.com/buiihuy)               | 20210443 | Phó nhóm    | Frontend           |
-| [Phạm Quang Huy](https://github.com/Huygiauten)         | 20210448 |             | Frontend           |
-| [Trần Ngọc Bảo](https://github.com/TranNgocBao12062003) | 20215529 |             | Frontend           |
-| [Vũ Minh Hiển](https://github.com/Minh-Hien2904)        | 20210324 |             | Report             |
-## Giới thiệu
-## Cài đặt
-1. Tải repository
-```
-git clone https://github.com/vepiot/Quan-Ly-Thu-Phi-Dong-Gop.git
-```
-2. Tải thư viện Django
-```
-pip install django
-```
-## Chạy phần mềm
-1. Chạy phần mềm
-```
-python manage.py runserver
-```
-Ứng dụng sẽ được chạy ở [http://localhost:3000](http://localhost:3000)
-## DEMO
-1. Sản phẩm demo: [DEMO](https://projectnmcnpm--sondt210750.repl.co/)
-2. Tài khoản thường
-Người dùng có thể tự tạo tài khoản ở phần Đăng ký
+SECRET_KEY = 'django-insecure-1v+fbc!rdrq=tbamx4myhnd!#n4ww2rkq1##qqnoxy+xwubf7d'
 
-![image](https://hackmd.io/_uploads/HJ8eAmYN6.png)
+DEBUG = True
 
-3. Tài khoản ADMIN (Đăng nhặp tại [ADMIN](https://projectnmcnpm--sondt210750.repl.co/admin)):
-- Tên tài khoản: `admin`
-- Mật khẩu: `admin123`
-### Giới thiệu phần mềm
-Phần mềm quản lý thu phí đóng góp cho tổ dân phố là một ứng dụng Web chạy bởi backend là [Django](https://www.djangoproject.com/start/overview/)
+ALLOWED_HOSTS = ['*']
 
-Trang quản trị Django: 
+AUTH_USER_MODEL = 'nmcnpmapp.CustomUser'
 
-![image](https://hackmd.io/_uploads/ryiPimYNp.png)
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = "/" 
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'nmcnpmapp',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'myproject.urls'
+
+LOGIN_REDIRECT_URL = 'home'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'myproject.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+CSRF_TRUSTED_ORIGINS = ['https://projectnmcnpm.sondt210750.repl.co','https://serval-main-lizard.ngrok-free.app',]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'vi'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
